231f4cd42d5f6d15d04f5c3dbc2bbf7fc0dad0b2
jorgeaveraf <jorgeaveraf@gmail.com>
2025-04-04 16:25:38 -0600

    fix of unused line

--- src/pages/ConfirmationPage.tsx
+++ src/pages/ConfirmationPage.tsx
@@ -1,5 +1,4 @@
 import { FC } from "react";
-import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 import { useSectionInView } from "../hooks/useSectionInView";
 import check from "../assets/check.png";
